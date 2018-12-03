30b62b663d455977264fb2b480860c74952bbfa6
Yegor Voronyansky <vrnsky@protonmail.ch>
2018-12-03 23:36:32 +0300

    Add pyplot csv parsing

---
+++ chapter16/high_lows.py
@@ -0,0 +1,39 @@
+﻿import csv
+from matplotlib import pyplot as plt
+from datetime import datetime
+
+filename = 'sitka_weather_2014.csv'
+
+with open(filename) as f:
+	reader = csv.reader(f)
+	highs = []
+	dates = []
+	lows = []
+	header_row = next(reader) #skip header
+	for row in reader:
+		try:
+			current_date = datetime.strptime(row[0], "%Y-%m-%d")
+			high = int(row[1])
+			low = int(row[3])
+		except ValueError:
+			print(current_date, 'missing data')
+		else:
+			dates.append(current_date)
+			highs.append(high)
+			lows.append(low)
+
+
+fig = plt.figure(dpi = 128, figsize=(10, 6))
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# Форматирование
+plt.title("Daily high and lows temperatures - 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+
+fig.autofmt_xdate()
+plt.ylabel("Temperature(F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
